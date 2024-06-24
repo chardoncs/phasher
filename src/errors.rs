6a9524766d2efc949bf552f9e16eac72bcf23dd7
Charles Dong <chardon_cs@proton.me>
2024-06-25 02:19:45 +0800

    Use password_hash and simple API

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,18 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub enum ErrorKind {
     HashGen,
     SaltDecoding,
+}
+
+impl Display for ErrorKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            Self::SaltDecoding => "Salt decoding",
+            Self::HashGen => "Hash generation",
+        })
+    }
 }
 
 #[derive(Debug)]
@@ -16,5 +27,11 @@ impl Error {
             kind,
             msg: msg.to_string(),
         }
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[error] {}: {}", self.kind, self.msg)
     }
 }
